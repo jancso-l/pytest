44c3144914c2ef2c6a40e97cc4f3607481117e14
jancso-l <61355854+jancso-l@users.noreply.github.com>
2026-02-17 19:45:56 +0100

    initial commit

---
+++ tests/test_my_funct.py
@@ -0,0 +1,19 @@
+import pytest
+import src.my_functions as my_functions
+
+def test_add():
+    result = my_functions.add(1, 4)
+    assert result == 5
+
+def test_add_strings():
+    result = my_functions.add("I like ", "burgers")
+    assert result == "I like burgers"
+
+def test_divide():
+    result = my_functions.divide(6, 3)
+    assert result == 2
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        my_functions.divide(10, 0)
+    
